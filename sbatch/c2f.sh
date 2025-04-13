92161688cd9198bdebbba459252817a5534018e2
Nithya <snnithya@gmail.com>
2025-04-13 17:45:19 -0400

    updates to handle slurm file systems

---
+++ sbatch/c2f.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=cat-vamp
+#SBATCH --partition=long                          
+#SBATCH --cpus-per-task=4                                
+#SBATCH --gres=gpu:1
+#SBATCH -C 40gb                                 
+#SBATCH --mem=32GB
+#SBATCH -o /network/scratch/n/nithya.shikarpur/vampnet/slurm-%j.out  # Write the log on scratch
+#SBATCH --time=2-00:00:00
+
+module --quiet load miniconda/3
+
+conda activate "vampnet"
+cd $HOME
+cd vampnet
+
+python scripts/exp/train.py --args.load /home/mila/n/nithya.shikarpur/scratch/checkpoints/vampnet/cat/c2f.yml 
+
+
--- scripts/exp/export.py
+++ scripts/exp/export.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 
 import argparse
 
@@ -12,11 +13,16 @@ parser.add_argument(
     help="model version to export. check runs/<name> for available versions"
 )
 
+parser.add_argument(
+    "--run_dir", type=str, default="runs",
+    help="directory where the run is stored"
+)
+
 args = parser.parse_args()
 name = args.name
 version = args.model
 
-run_dir = Path(f"runs/{name}")
+run_dir = Path(f"{args.run_dir}/{name}")
 repo_dir = Path("models/vampnet")
 
 for part in ("coarse", "c2f"):
@@ -24,13 +30,13 @@ for part in ("coarse", "c2f"):
     outdir.mkdir(parents=True, exist_ok=True)
     outpath = outdir / f"{part}.pth"
     path = run_dir / part / version / "vampnet" / "weights.pth"
-    path.rename(outpath)
+    shutil.copy(path, outpath)
     print(f"moved {path} to {outpath}")
 
 from huggingface_hub import Repository
 repo = Repository(str(repo_dir))
 print(f"pushing {repo_dir} to {name}")
 repo.push_to_hub(
-    commit_message=f"add {name}"
+    commit_message=f"add {name}",
 )
 print("done!!! >::0")
